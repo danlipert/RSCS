2e6d6627ae8c5136cfab5d3c31c22178de03e0da
Ozzy <96839831+zerocousin@users.noreply.github.com>
2024-06-23 11:52:09 +0200

    Structure fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "../components/providers";
+import { Providers } from '@/components/providers';
 import '@rainbow-me/rainbowkit/styles.css';
 import "./globals.css";
 
